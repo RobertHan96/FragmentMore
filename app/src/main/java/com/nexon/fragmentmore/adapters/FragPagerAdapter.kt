b47bf89b5374fc166f4a99120b2874e2d0a52244
RobertHan96 <yshan4329@gmail.com>
2020-04-20 21:01:37 +0900

    9th commit : 뷰페이저 어댑터 클래스 변경 - 3개의 페이지 연결 및 페이징 처리 - return when 문법으로 페이징 처리 코드 최적화

--- app/src/main/java/com/nexon/fragmentmore/adapters/FragPagerAdapter.kt
+++ app/src/main/java/com/nexon/fragmentmore/adapters/FragPagerAdapter.kt
@@ -4,20 +4,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.PagerAdapter
+import com.nexon.fragmentmore.fragments.BoardFragment
 import com.nexon.fragmentmore.fragments.GameListFragment
 import com.nexon.fragmentmore.fragments.MainFragment
 
 class FragPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
-        if (position == 0) {
-            return  MainFragment()
-        } else {
-            return GameListFragment()
+        return when(position){
+            0 -> MainFragment()
+            1 -> GameListFragment()
+            else -> BoardFragment()
         }
     }
 
     override fun getCount(): Int {
-        return  2
+        return  3
     }
 
 
