d1e4e4c4ceee8487f5e08f35a0b9d491b4165ff7
RobertHan96 <yshan4329@gmail.com>
2020-04-20 20:57:40 +0900

    8th commit : 거래게시판 어댑터를 프래그먼트와 연결, 초기화

---
+++ app/src/main/java/com/nexon/fragmentmore/fragments/BaseFragment.kt
@@ -0,0 +1,17 @@
+package com.nexon.fragmentmore.fragments
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+    var mContext: Context? = null
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mContext = activity
+    }
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
--- app/src/main/java/com/nexon/fragmentmore/fragments/BoardFragment.kt
+++ app/src/main/java/com/nexon/fragmentmore/fragments/BoardFragment.kt
@@ -1,7 +1,50 @@
 package com.nexon.fragmentmore.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Adapter
 import androidx.fragment.app.Fragment
+import com.nexon.fragmentmore.R
+import com.nexon.fragmentmore.adapters.PostAdapter
+import com.nexon.fragmentmore.datas.Post
+import kotlinx.android.synthetic.main.fragment_board.*
 
-class BoardFragment : Fragment() {
-    
+class BoardFragment : BaseFragment() {
+    var mPostAdapter : PostAdapter? = null
+    var mPostList = ArrayList<Post>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_board, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        mPostList.add(Post("삼성전자", 100, "한영신"))
+        mPostList.add(Post("네이버", 500, "이정재"))
+        mPostList.add(Post("NCSOFT", 2500, "엘론 머스크"))
+        mPostList.add(Post("LG전자", 10, "김홀동"))
+        mPostList.add(Post("데브캣", 90, "홍길동"))
+        mPostList.add(Post("선데이토즈", 145, "이미란"))
+        mPostList.add(Post("애플", 3789, "김보성"))
+        mPostList.add(Post("마이크로소프트", 789, "손전쟁"))
+        mPostList.add(Post("SPC", 77, "신기루"))
+        mPostAdapter = PostAdapter(mContext!!, R.layout.post_list_item, mPostList)
+        boardListView.adapter = mPostAdapter
+    }
+
 }
