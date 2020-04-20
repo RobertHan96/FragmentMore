c7fd94e65034facb15983097607ad195ffaba8bc
RobertHan96 <yshan4329@gmail.com>
2020-04-20 20:45:27 +0900

    7th commit : 거래게시판 어댑터 기초 작업 완료

---
+++ app/src/main/java/com/nexon/fragmentmore/adapters/PostAdapter.kt
@@ -0,0 +1,38 @@
+package com.nexon.fragmentmore.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.nexon.fragmentmore.R
+import com.nexon.fragmentmore.datas.App
+import com.nexon.fragmentmore.datas.Post
+
+class PostAdapter (context: Context, resID : Int, list: ArrayList<Post>) : ArrayAdapter<Post>(context, resID, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        tempRow?.let{ 
+            // null이 아닐때
+        }.let {
+            // null일때
+            tempRow = inf.inflate(R.layout.post_list_item, null)
+        }
+
+//    위에서 이미 null 체크가 끝났으므로 내가 가져올 리스트 아이템은 !!를 사용해 강제 언랩핑해서 사용 준비
+        val row = tempRow!!
+//     findViewById를 할때는 찾을 객체의 속성을 <> 안에 정확히 입력해야 객체를 가져올 수 있음
+
+
+        return  row
+    }
+
+
+}
+
